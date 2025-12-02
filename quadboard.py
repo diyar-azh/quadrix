883a840daebb53c1d6c116a2a56bd6c3e08c7811
Diyar Azhir <diyarazhir@outlook.com>
2025-12-02 23:00:00 +0330

    Update quadboard.py

--- quadboard.py
+++ quadboard.py
@@ -1,4 +1,10 @@
 class quadboard():
-  def __init__(self, rw, cl):
-    self.rw = rw
-    self.cl = cl
+  def __init__(self):
+    self.rw = 7
+    self.cl = 7
+
+  def Capture(rw, cl):
+    pass
+    
+  def SaveCapture(rw, cl):
+    x = (rw * 7) + cl
